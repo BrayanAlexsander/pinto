5986843acea7d299b6550ac9516016fb73fc8f47
ComputerSenai <brayan.oliveira@es.estudante.senai.com>
2022-03-25 17:07:53 -0300

    Initial commit

---
+++ src/io.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;{
+
+public class exercicio13<pesoIdealh> {
+    Scanner Scanner = new Scanner(System.in);
+    System.out.print("Informe sua altura:  ");
+    Double altura = Scanner.nextDouble();
+    double fatorMutiplicadorH = 72.7;
+    byte fatorSubtracaoH = 58;
+    Double pesoIdealh = (fatorMutiplicadorH * altura) - fatorSubtracaoH;
+    System.out.print("Seu peso ideal é Masculino: " + pesoIdealh);
+    double fatorMmutiplicadorM = 62*1;
+    double fatorSubtracao =44.7;
+    Double pesoIdealm = (62.1 * altura) - 44.7;
+        System.out.print("Seu peso ideal é feminino: " + pesoIdealm);
+        Scanner.close();
+    }
+ }
